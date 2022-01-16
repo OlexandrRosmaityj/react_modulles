c339c68e3c4bcc97c3f29f56f6bf7d13d1f37425
OlexandrRosmaityj <rosmaityj@gmail.com>
2022-01-16 19:39:22 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Users from "./Componnents/Users/Users";
+import Posts from "./Componnents/Posts/Posts";
+import Comments from "./Componnents/Comments/Comments";
+
 
 function App() {
   return (
-    <div>
+      <div>
+        <div className={'div'}>
+          <Users/>
+          <Posts/>
+        </div>
+        <Comments/>
 
-    </div>
+      </div>
   );
 }
 
