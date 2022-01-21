0cd867030e1b20e981883fa1df05510d0e2d9841
OlexandrRosmaityj <rosmaityj@gmail.com>
2022-01-22 01:08:31 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import {useState} from "react";
+import React from 'react';
 
-
-import Users from "./components/Users/Users";
-import UsersDetalis from "./components/UsersDetalis/UsersDetalis";
-import Posts from "./components/Posts/Posts";
-import style from './App.module.css'
-import {postService} from "./service/post.service";
-
-
-function App() {
-    const [user, setUser] = useState(null);
-    const [userId, setUserId] = useState(null);
-    const [posts, setPosts] = useState([]);
-
-
-
-
-    const getUser = (user) => {
-        setUser(user)
-        setPosts([])
-    }
-const  getUserId =(id)=>{
-postService.getByUserId(id).then(value => setPosts([...value]))
-
-}
-
+const App = () => {
     return (
-
         <div>
-            <div className={style.wrap}>
-
-                <Users getUser={getUser}/>
-
-                {user&&  <UsersDetalis user={user} getUserId ={getUserId}/>}
-
-            </div>
-
-            {!!posts.length&&<Posts posts={posts}/>}
 
         </div>
     );
-}
+};
 
 export default App;
