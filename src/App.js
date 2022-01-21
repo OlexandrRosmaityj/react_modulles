53eb9e86f5f21c12c9c8c7e08430306fc3d88e37
OlexandrRosmaityj <rosmaityj@gmail.com>
2022-01-21 16:16:47 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import Users from "./components/Users/Users";
+import UsersDetalis from "./components/UsersDetalis/UsersDetalis";
+import Posts from "./components/Posts/Posts";
+import style from './App.module.css'
 
 function App() {
   return (
     <div>
+      <div className={style.wrap}>
+
+        <Users/>
+
+        <UsersDetalis/>
+
+      </div>
+
+      <Posts/>
 
     </div>
   );
