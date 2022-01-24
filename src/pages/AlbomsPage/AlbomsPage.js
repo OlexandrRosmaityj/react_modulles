e2be6c8c8c88dde88dd6a5ad65931906809267fe
OlexandrRosmaityj <rosmaityj@gmail.com>
2022-01-24 16:00:58 +0200

    new

---
+++ src/pages/AlbomsPage/AlbomsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Outlet} from "react-router-dom";
+
+const AlbomsPage = () => {
+    return (
+        <div>
+            AlbomsPage
+            <Outlet/>
+        </div>
+    );
+};
+
+export default AlbomsPage;
