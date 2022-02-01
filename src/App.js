b0397a0108cbe3cbe9ffaaa25d5934b032588a59
OlexandrRosmaityj <91395352+OlexandrRosmaityj@users.noreply.github.com>
2022-02-02 00:53:01 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, {useState} from 'react';
+
 import {Cars, Form} from "./components";
 
 const App = () => {
-    const [cars, setCars] = useState([]);
 
-
-    const getFormData =(data)=>{
-        setCars([...cars,{id:new Date().getTime(),...data}])
-    }
-
-    const  getCarID =(id)=>{
-        setCars(cars.filter(car=>car.id !== id))
-    }
 
     return (
         <div>
-            <Form getFormData ={getFormData}/>
-            <Cars cars ={cars}  getCarID={getCarID}/>
+            <Form/>
+            <Cars/>
         </div>
     );
 };
