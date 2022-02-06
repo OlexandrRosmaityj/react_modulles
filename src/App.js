ae6e4e2c3a1a527e2a6504f840677ce935b1abd6
OlexandrRosmaityj <91395352+OlexandrRosmaityj@users.noreply.github.com>
2022-02-06 06:08:18 +0200

    new1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPage, HomePage, NotFoundPage, PostsPage, UsersPage} from "./pages";
+import {Layout} from "./components/Layout/Layout";
+import {SinglePostPage} from "./pages/SinglePostPage/SinglePostPage";
+
 const App = () => {
     return (
-        <div>
+        <>
+            <div>
+                <Routes>
+                    <Route path={'/'} element={<Layout/>}>
 
-        </div>
+                    <Route index element={<HomePage/>}/>
+                    <Route path={'users'} element={<UsersPage/>}/>
+
+                    <Route path={'posts'}  element={<PostsPage/>}>
+                        <Route path={':id'}  element={<SinglePostPage/>}/>
+                    </Route>
+
+                    <Route path={'about'} element={<AboutPage/>}/>
+                    <Route path={'*'} element={<NotFoundPage/>}/>
+
+                    </Route>
+                </Routes>
+            </div>
+        </>
     );
 };
 
