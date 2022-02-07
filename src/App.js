4448d2b2bc9b0014f355eb5fee6c2a42e415973f
OlexandrRosmaityj <91395352+OlexandrRosmaityj@users.noreply.github.com>
2022-02-08 01:46:11 +0200

    new1

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPage, HomePage, NotFoundPage, PostsPage, UsersPage} from "./pages";
 import {Layout} from "./components/Layout/Layout";
 import {SinglePostPage} from "./pages/SinglePostPage/SinglePostPage";
+import {LoginPage} from "./pages/LoginPage/LoginPage";
+import {RequierAuth} from "./hoc/RequierAuth";
+import AuthProvider from "./hoc/AuthProvider";
 
 const App = () => {
     return (
-        <>
+        <AuthProvider>
             <div>
                 <Routes>
                     <Route path={'/'} element={<Layout/>}>
 
                     <Route index element={<HomePage/>}/>
-                    <Route path={'users'} element={<UsersPage/>}/>
+                    <Route path={'users'} element={
+                        <RequierAuth>
+                        <UsersPage/>
+                    </RequierAuth>}/>
 
                     <Route path={'posts'}  element={<PostsPage/>}>
                         <Route path={':id'}  element={<SinglePostPage/>}/>
                     </Route>
 
                     <Route path={'about'} element={<AboutPage/>}/>
+                    <Route path={'login'} element={<LoginPage/>}/>
                     <Route path={'*'} element={<NotFoundPage/>}/>
 
                     </Route>
                 </Routes>
             </div>
-        </>
+        </AuthProvider>
     );
 };
 
